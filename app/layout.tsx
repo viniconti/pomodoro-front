2373056603fa59c8eb039947bfdc04a065da6fe5
Vinicius Conti <vinicius.sconti@eaportal.org>
2026-06-01 17:15:35 -0300

    Tela de login

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Allerta, Anuphan } from "next/font/google";
+import "./globals.css";
+import "./styles/left-side.css"
+import "./styles/right-side.css"
+
+const allerta = Allerta({
+  variable: "--font-allerta",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const anuphan = Anuphan({
+  variable: "--font-anuphan",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Pomodoro Timer",
+  description: "Um site de pomodoro aí",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="pt-br"
+      className={`${allerta.variable} ${anuphan.variable}`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
